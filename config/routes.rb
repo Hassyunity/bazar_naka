c38895305f251d2882564502dfd3685f31953311
Hassy Tsihoarana <78786423+Hassyunity@users.noreply.github.com>
2024-07-12 21:45:59 +0300

    Revert "Revert "add solidaire section WIP""

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sections/index'
+  get 'sections/create'
+  get 'sections/update'
+  get 'sections/delete'
   # get 'pages/home'
   # get 'pages/comment_payer'
   # get 'pages/nouvaute'
